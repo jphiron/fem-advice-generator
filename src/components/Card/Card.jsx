9cde18b36bd6fa6568bdd895832dd01a76889030
Jack Hiron <jackphiron@gmail.com>
2022-05-17 13:17:50 +0100

    Finish styling Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,19 @@
-import { Container, Label, Body, Button } from "./Card.styles";
+import { Container, Label, Quote, Button } from "./Card.styles";
 import DiceIcon from "../../assets/images/icon-dice.svg";
+import MobileSeparator from "../../assets/images/pattern-divider-mobile.svg";
 import DesktopSeparator from "../../assets/images/pattern-divider-desktop.svg";
 
 const Card = () => {
   return (
     <Container>
-      <Label>Advice #</Label>
-      <Body>
-        Turn jeans inside out when washing them to help preserve their colour.
-      </Body>
-      <img src={DesktopSeparator} alt="" />
+      <Label>Advice #117</Label>
+      <Quote>
+        “It is easy to sit up and take notice, what's difficult is getting up and taking action.”
+      </Quote>
+      <picture>
+        <source srcset={DesktopSeparator} media="(min-width: 570px)"/>
+        <img src={MobileSeparator} alt="" />
+      </picture>
       <Button>
         <img src={DiceIcon} alt="Dice icon" />
       </Button>
--- src/components/Card/Card.styles.jsx
+++ src/components/Card/Card.styles.jsx
@@ -1,20 +1,62 @@
 import styled from "styled-components";
+import { colors } from "../../style-variables";
+
+const { neonGreen, lightCyan, grayishBlue, darkGrayishBlue, darkBlue } = colors;
 
 const Container = styled.div`
   display: flex;
+  position: relative;
   flex-flow: column nowrap;
   align-items: center;
-  justify-content: space-evenly;
-  background: hsl(217, 19%, 24%);
-  width: 450px;
-  height: 250px;
-  padding: 2rem;
-`
+  justify-content: space-around;
+  background: ${darkGrayishBlue};
+  max-width: 450px;
+  padding: 3rem 2.5rem 4rem 2.5rem;
+  border-radius: 15px;
 
-const Label = styled.h2``;
+  @media screen and (max-width: 410px) {
+    padding: 2rem 1.1rem 3rem;
+  }
+`;
 
-const Body = styled.p``;
+const Label = styled.h2`
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  color: ${neonGreen};
+  letter-spacing: 3px;
+  margin-bottom: 1.5rem;
+`;
 
-const Button = styled.button``;
+const Quote = styled.p`
+  margin-bottom: 2.5rem;
+  text-align: center;
+  font-size: 28px;
+  color: ${lightCyan};
 
-export { Container, Label, Body, Button };
+  @media screen and (max-width: 410px) {
+    margin-bottom: 2rem;
+    font-size: 28px;
+  }
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: -30px;
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  outline: none;
+  border: none;
+  background: ${neonGreen};
+  cursor: pointer;
+  transition: box-shadow 0.2s ease-out;
+
+  &:hover {
+    box-shadow: 0 0 30px ${neonGreen};
+  }
+`;
+
+export { Container, Label, Quote, Button };
