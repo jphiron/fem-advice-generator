31b083fc980d3dc04d5bc25f6b5a900e4bb893ef
Jack Hiron <jackphiron@gmail.com>
2022-05-17 22:17:59 +0100

    Add hidden h1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Card } from './components/Card/Card';
 function App() {
   return (
     <main className="App">
+      <h1 className="sr-only">Advice generator</h1>
       <Card />
     </main>
   );
